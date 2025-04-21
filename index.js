b9d40922717b7e9acb7b9d35559712e6e056236c
Almas <gdocs34@gmail.com>
2025-04-21 09:33:41 +0300

    Short summary
    Интегрировано краткое резюме с помощью xai api

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
 const { createClient, LiveTranscriptionEvents } = require("@deepgram/sdk");
-const fetch = require("cross-fetch");
 const { spawn } = require('child_process');
 const Mic = require('mic');
+const OpenAI = require('openai');
+const fs = require('node:fs');
+const readline = require('readline');
 
 require('dotenv').config();
 
+const newFileName = Date.now();
+
+readline.emitKeypressEvents(process.stdin);
+process.stdin.on('keypress', async (_, key) => {
+    // console.log('got "keypress"', ch, key);
+    if (key && key.name == 'space') {
+        // process.stdin.pause();
+        const summaryText = await getSummary();
+        await fs.appendFileSync(`${newFileName}-summary.log`, summaryText);
+        process.exit();
+    }
+});
+process.stdin.setRawMode(true);
+
+const getSummary = async () => {
+    const client = new OpenAI({
+        apiKey: process.env.XAI_API_KEY,
+        baseURL: "https://api.x.ai/v1",
+    });
+    const dialogue = fs.readFileSync(`${newFileName}.log`, 'utf8');
+    const completion = await client.chat.completions.create({
+        model: "grok-3-beta",
+        messages: [
+            { role: "system", content: `Напиши краткое резюме следующего диалога: ${dialogue}` }
+        ]
+    });
+    const summary = await completion.choices[0]?.message?.content;
+    return summary;
+}
+
 const live = async () => {
-    const deepgram = createClient( process.env.DEEPGRAM_API_KEY);
+    const deepgram = createClient(process.env.DEEPGRAM_API_KEY);
+
     // Create a websocket connection to Deepgram
     const connection = deepgram.listen.live({
         punctuate: true,
@@ -26,17 +59,30 @@ const live = async () => {
             const speaker = data.channel?.alternatives[0]?.words[0]?.speaker;
             const transcript = data.channel?.alternatives[0]?.transcript;
             if (speaker !== undefined) {
-                console.log(`Персона ${parseInt(speaker) + 1} говорит: ${transcript}`);
+                // console.log(`Персона ${parseInt(speaker) + 1}: ${transcript}`);
+                const content = `Персона ${parseInt(speaker) + 1}: ${transcript}`;
+                fs.appendFile(`${newFileName}.log`, `${content}\n`, err => {
+                    if (err) {
+                        console.error(err);
+                    } else {
+                        console.log(content);
+                    }
+                });
             }
         });
+
         // Listen for any metadata received from Deepgram and write it to the console.
         connection.on(LiveTranscriptionEvents.Metadata, (data) => {
             // console.dir(data, { depth: null });
         });
+
         // Listen for the connection to close.
         connection.on(LiveTranscriptionEvents.Close, () => {
             console.log("Connection closed.");
         });
+
+
+
         const mic = Mic({
             rate: '44100',
             channels: '1',
